5fcaeeaa715f475b6ea470166909a6e56a8a2205
Pamudu Wijayabandara <pamudu884@gmail.com>
2025-06-06 22:49:24 +0530

    Update productServiceimpl.java

--- src/main/java/com/example/productCRUD/Service/productServiceimpl.java
+++ src/main/java/com/example/productCRUD/Service/productServiceimpl.java
@@ -5,24 +5,51 @@ import com.example.productCRUD.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class productServiceimpl implements ProductService {
 
-
     @Autowired
     private ProductRepo productRepo;
 
+    // Create
     @Override
-    public Product addProduct(Product product){
+    public Product addProduct(Product product) {
         return productRepo.save(product);
     }
 
-
+    // Read all
     @Override
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productRepo.findAll();
+    }
+
+    // Read one by ID
+    @Override
+    public Optional<Product> getProductById(int id) {
+        return productRepo.findById(id);
+    }
+
+    // Update
+    @Override
+    public Product updateProduct(int id, Product updatedProduct) {
+        Optional<Product> optionalProduct = productRepo.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product existingProduct = optionalProduct.get();
+            existingProduct.setName(updatedProduct.getName());
+            existingProduct.setPrice(updatedProduct.getPrice());
+            // Add more fields if applicable
+            return productRepo.save(existingProduct);
+        } else {
+            throw new RuntimeException("Product not found with ID: " + id);
+        }
+    }
+
+    // Delete
+    @Override
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
     }
 }
